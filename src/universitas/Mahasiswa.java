784e44928f1cd9dcb085e60d08877b21e12a57f1
MuhmmdKoko <someanncat@gmail.com>
2023-06-13 08:45:17 +0800

    Melengkapi class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -15,4 +15,28 @@ public class Mahasiswa {
     String setDetail(){
         return npm + nama + prodi;
     }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
 }
