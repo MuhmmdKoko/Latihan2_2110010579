6a810f8cef350afbc50f29f6aeaba1372278ae2f
MuhmmdKoko <someanncat@gmail.com>
2023-06-13 08:42:26 +0800

    Membuat class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/MahasiswaBeraksi.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universitas;
+
+/**
+ *
+ * @author USER
+ */
+public class MahasiswaBeraksi {
+    
+}
