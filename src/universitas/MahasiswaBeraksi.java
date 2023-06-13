a46318d3ae13595b84336ea2c2f8714189a674a1
MuhmmdKoko <someanncat@gmail.com>
2023-06-13 08:50:02 +0800

    Menjalankan program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -2,6 +2,10 @@ package universitas;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
+        //membuat objek
+        Mahasiswa koko = new Mahasiswa("2110010579", " Muhammad Koko Saputro", " Teknik Informatika");
         
+        //memanggil method getDetail
+        System.out.println(koko.setDetail());
     }
 }
